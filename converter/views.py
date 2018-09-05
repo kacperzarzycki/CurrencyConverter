1c1129fcd979b8d7b5f139c6c6b4dbe1de093be0
Kacper <kacperzarzycki98@gmail.com>
2018-09-05 11:01:16 +0200

    Basic template

--- converter/views.py
+++ converter/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+	word = 'hello!'
+	context = {'word': word}
+	return render(request, 'converter/index.html', context)
