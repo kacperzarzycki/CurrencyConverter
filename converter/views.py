ba0e6ac1900443d63b4ddc31914806416d684818
Kacper <kacperzarzycki98@gmail.com>
2018-09-06 11:02:03 +0200

    CurrencyConverter old date test

--- converter/views.py
+++ converter/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from currency_converter import CurrencyConverter
+from datetime import date
+
+cc = CurrencyConverter()
 
 def index(request):
-	word = 'hello!'
-	context = {'word': word}
+	currency_date = date(2018, 3, 20)
+	first_currency = 'EUR'
+	second_currency = 'PLN'
+	amount = 785
+	converted_amount = round(cc.convert(amount, first_currency, second_currency, date=currency_date), 3)
+	context = {'first_currency': first_currency, 'second_currency': second_currency, 'amount': amount, 'converted_amount': converted_amount, 'currency_date': currency_date}
 	return render(request, 'converter/index.html', context)
