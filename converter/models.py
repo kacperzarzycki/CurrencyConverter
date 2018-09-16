16922ef055e8c74e74652afb4e4bd880110dad9d
Kacper <kacperzarzycki98@gmail.com>
2018-09-16 13:05:53 +0200

    Set default values

--- converter/models.py
+++ converter/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Data(models.Model):
-	currency1 = models.CharField(max_length=3)
-	currency2 = models.CharField(max_length=3)
-	amount = models.IntegerField()
+	currency1 = models.CharField(max_length=3, default='PLN')
+	currency2 = models.CharField(max_length=3, default='EUR')
+	amount = models.IntegerField(default=100)
 	date = models.DateField(default='2018-03-03')
